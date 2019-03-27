9ee24e620e39ada84c9816475777e55b3226b3ca
wei feng <theweifeng@gmail.com>
2019-03-27 14:10:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(array, name)
   array << name
-  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
 def now_serving(deli)
